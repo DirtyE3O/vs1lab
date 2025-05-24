afb7626aae59799e3725137b4b16df80e7569401
MertSaracoglu03 <mertsaracoglu2003@icloud.com>
2025-05-24 15:04:15 +0200

    3.2.2 b

--- Aufgabe3/gta_v3/public/javascripts/geotagging.js
+++ Aufgabe3/gta_v3/public/javascripts/geotagging.js
@@ -10,21 +10,48 @@
 console.log("The geoTagging script is going to start...");
 
 
-
 /**
  * TODO: 'updateLocation'
  * A function to retrieve the current location and update the page.
  * It is called once the page has been fully loaded.
  */
 function updateLocation() {
-    LocationHelper.findLocation((helper) => {
-        var latitude = helper.latitude;
-        var longitude = helper.longitude;
 
-        var tagLatitude = document.getElementById("tag-latitude");
-        var tagLongitude = document.getElementById("tag-longitude");
-        var searchLatitude = document.getElementById("search-latitude");
-        var searchLongitude = document.getElementById("search-longitude");
+    const tagLatitude = document.getElementById("tag-latitude");
+    const tagLongitude = document.getElementById("tag-longitude");
+    const searchLatitude = document.getElementById("search-latitude");
+    const searchLongitude = document.getElementById("search-longitude");
+
+    const needsLocation = !tagLatitude.value || !tagLongitude.value ||
+        !searchLatitude.value || !searchLongitude.value;
+
+    let latitude;
+    let longitude;
+
+    if (!needsLocation) {
+        latitude = tagLatitude.value;
+        longitude = tagLongitude.value;
+
+
+        let image = document.querySelector("#mapView");
+        let caption = document.querySelector("#map span");
+
+        if (image) {
+            image.remove();
+        }
+        if (caption) {
+            caption.remove();
+        }
+        let map = new MapManager();
+        map.initMap(latitude, longitude);
+        map.updateMarkers(latitude, longitude);
+        return;
+
+    }
+
+    LocationHelper.findLocation(function (helper) {
+        latitude = helper.latitude;
+        longitude = helper.longitude;
 
         if (tagLatitude != null) {
             tagLatitude.value = latitude;
@@ -37,15 +64,6 @@ function updateLocation() {
         }
         if (searchLongitude != null) {
             searchLongitude.value = longitude;
-        }
-        let image = document.querySelector("#mapView");
-        let caption = document.querySelector("#map span");
-
-        if ( image) {
-            image.remove();
-        }
-        if (caption) {
-            caption.remove();
         }
         let map = new MapManager();
         map.initMap(latitude, longitude);
